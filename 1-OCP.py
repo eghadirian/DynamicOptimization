dbdbbf1905e20df177ca5e3621585df846dd31d8
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-04-27 22:48:54 -0400

    update non-linearities

--- 1-OCP.py
+++ 1-OCP.py
@@ -1,5 +1,6 @@
 from rockit import *
 import matplotlib.pyplot as plt
+import numpy as np 
 
 ocp = Ocp(T=10)
 x1 = ocp.state()
@@ -8,13 +9,14 @@ u = ocp.control(order=0)
 p = ocp.control(order=1)
 z = ocp.algebraic()
 
-ocp.set_der(x1, x1 - z*x2**2 + u)
-ocp.set_der(x2, x1+p)
+ocp.set_der(x1, x1 - z*x2**2 + np.sin(u))
+ocp.set_der(x2, x1 + p + np.exp(z))
 ocp.add_alg(z-(1 - x1**3)**2)
 
-ocp.add_objective(ocp.integral(x2**2 + x1**2 + u**2 - p**2 + z**2))
+ocp.add_objective(ocp.integral((z-0.5)**2))
 
 ocp.subject_to(-1 <= (u <= 1))
+ocp.subject_to(-3 <= p)
 ocp.subject_to(-0.2 <= (u*p <= 0.2))
 ocp.subject_to(-0.25 <= (x1 <= 1))
 ocp.subject_to(-0.3 <=(ocp.der(p)<=0.3))
@@ -30,8 +32,10 @@ tsc, x1c = sol.sample(x1, grid='integrator')
 tsc, x2c = sol.sample(x2, grid='integrator')
 tsc, uc = sol.sample(u, grid='integrator')
 tsc, pc = sol.sample(p, grid='integrator')
+tsc, zc = sol.sample(z, grid='integrator')
 plt.plot(tsc, x1c, '-o', label='state 1')
 plt.plot(tsc, x2c, '-^', label='state 2')
+plt.plot(tsc, zc, '-v', label='algebraic')
 plt.plot(tsc, uc, '-.', label='control 1')
 plt.plot(tsc, pc, '--', label='control 2')
 plt.xlabel('time, s')
